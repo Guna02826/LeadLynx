e9a5ceaf2bb59b2c639ab74559b15bf79933fe9a
Gunasekaraselvapandian <gssp02826@gmail.com>
2025-11-05 13:17:28 +0530

    #

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get("/api/health", (req, res) => {
   res.json({ ok: true, time: new Date() });
 });
 
-appp.use(cors());
+app.use(cors());
 
 app.use("/api/users", userRoute);
 app.use("/api/leads", leadRoute);
